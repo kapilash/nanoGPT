0af145a54ce54b39802d142b0aa1ad36440da5ac
Sai Hemanth Kapila <kapilash@outlook.com>
2025-03-03 09:29:16 +0530

    print

--- model.py
+++ model.py
@@ -187,6 +187,7 @@ class GPT(nn.Module):
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
+            print("TARGETS IS NONE:", x.shape)
             logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
             loss = None
 
